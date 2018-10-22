823b0b23ed0754239f368695070da61fe9d05aa1
Cyrus Cleary <zeusofthecrows+github@gmail.com>
2018-10-22 09:56:21 +0100

    I don't know why this does anything

---
+++ Contract1.py
@@ -0,0 +1,36 @@
+import pygame
+from pygame.locals import *
+pygame.init()
+
+clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode((700, 533))
+screen.fill((33, 33, 39))
+pygame.display.set_caption('Contract #1 Demonstration')
+image = pygame.image.load('Images/Floor.jpg')
+colourToReplace = (128, 0, 128)
+
+
+def replace_colour(colour_to_replace, new_colour):
+    for x in range(700):
+        for y in range(533):
+            current_pixel_old = image.get_at((x, y))
+            current_pixel_new = current_pixel_old
+            # if current_pixel_old.r == colour_to_replace[0]:
+            image.set_at((x, y), current_pixel_new)
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
+            running = False
+
+    screen.blit(image, (0, 0))
+
+    replace_colour(colourToReplace, (0, 0, 0))
+
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
