d16e25c6cacc133914efd3fa288fa00dcf94c70f
Cyrus Cleary <zeusofthecrows+github@gmail.com>
2018-10-23 10:47:22 +0100

    This morning

--- Contract1.py
+++ Contract1.py
@@ -7,10 +7,15 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((700, 533))
 screen.fill((33, 33, 39))
 pygame.display.set_caption('Contract #1 Demonstration')
-image = pygame.image.load('Images/Floor.jpg')
-colourToReplace = (128, 0, 128)
+image = pygame.image.load('Images/Image1.png')
+colourToReplace = pygame.Color(255, 0, 135)
 
 
+def replace_colour(colour_to_replace, new_colour):
+    pygame.PixelArray.replace(colour_to_replace, new_colour)
+
+
+'''
 def replace_colour(colour_to_replace, new_colour):
     for x in range(700):
         for y in range(533):
@@ -18,7 +23,7 @@ def replace_colour(colour_to_replace, new_colour):
             current_pixel_new = current_pixel_old
             # if current_pixel_old.r == colour_to_replace[0]:
             image.set_at((x, y), current_pixel_new)
-
+            '''
 
 running = True
 while running:
@@ -28,7 +33,7 @@ while running:
 
     screen.blit(image, (0, 0))
 
-    replace_colour(colourToReplace, (0, 0, 0))
+    replace_colour(colourToReplace, pygame.Color(0, 0, 0))
 
     pygame.display.update()
     clock.tick(60)
