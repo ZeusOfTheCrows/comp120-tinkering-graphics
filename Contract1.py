c210af3ec2b8e833c2e297b0cae52413945b595f
ZeusOfTheCrows <zeusofthecrows+github@gmail.com>
2018-10-24 11:00:01 +0100

    Tidied up and added few comments

--- Contract1.py
+++ Contract1.py
@@ -4,41 +4,33 @@ pygame.init()
 
 clock = pygame.time.Clock()
 
-image = pygame.image.load('Images/Image1.png')
-colourToReplace = pygame.Color(255, 0, 135)
-newColours = {}
-screen = pygame.display.set_mode((image.get_width(), image.get_height()))
+imageToRecolour = pygame.image.load('Images/Image1.png')
+colourToBeReplaced = pygame.Color(128, 0, 128)
+newColours = {'Blood': (102, 0, 0), 'Mud': (144, 108, 63)}
+screen = pygame.display.set_mode((imageToRecolour.get_width(), imageToRecolour.get_height()))
 screen.fill((33, 33, 39))
 pygame.display.set_caption('Contract #1 Demonstration')
 
 
-'''
-def replace_colour():
-    for x in range(700):
-        for y in range(533):
-            current_pixel_old = image.get_at((x, y))
-            current_pixel_new = current_pixel_old
-            # if current_pixel_old.r == colour_to_replace[0]:
-            image.set_at((x, y), current_pixel_new)
-            '''
-
-
-def replace_colour(colour_to_replace, new_colour, image):
+def replace_colour(colour_to_be_replaced, replacement_colour, image):
     for x in range(0, image.get_width()):
         for y in range(0, image.get_height()):
-            if image.get_at((x, y)) == colour_to_replace:
-                image.set_at((x, y), new_colour)
+            if image.get_at((x, y)) == colour_to_be_replaced:
+                image.set_at((x, y), replacement_colour)
     return image
 
 
 running = True
-while running:
+while running:  # Main game loop
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
             running = False
+        elif event.type == KEYUP and event.key == K_b:  # Press b for blood coloured splash
+            replace_colour(colourToBeReplaced, newColours['Blood'], imageToRecolour)
+        elif event.type == KEYUP and event.key == K_m:  # Press m for mud coloured splash
+            replace_colour(colourToBeReplaced, newColours['Mud'], imageToRecolour)
 
-    replace_colour(colourToReplace, pygame.Color(0, 0, 0), image)
-    screen.blit(image, (0, 0))
+    screen.blit(imageToRecolour, (0, 0))
 
     pygame.display.update()
     clock.tick(60)
