174aac246c51504c3e358b0614598625f1c07592
Cyrus Cleary <zeusofthecrows+github@gmail.com>
2018-10-24 09:44:55 +0100

    Commented out weird code and added working Colour replacer

--- Contract1.py
+++ Contract1.py
@@ -4,19 +4,16 @@ pygame.init()
 
 clock = pygame.time.Clock()
 
-screen = pygame.display.set_mode((700, 533))
-screen.fill((33, 33, 39))
-pygame.display.set_caption('Contract #1 Demonstration')
 image = pygame.image.load('Images/Image1.png')
 colourToReplace = pygame.Color(255, 0, 135)
-
-
-def replace_colour(colour_to_replace, new_colour):
-    pygame.PixelArray.replace(colour_to_replace, new_colour)
+newColours = {}
+screen = pygame.display.set_mode((image.get_width(), image.get_height()))
+screen.fill((33, 33, 39))
+pygame.display.set_caption('Contract #1 Demonstration')
 
 
 '''
-def replace_colour(colour_to_replace, new_colour):
+def replace_colour():
     for x in range(700):
         for y in range(533):
             current_pixel_old = image.get_at((x, y))
@@ -25,15 +22,23 @@ def replace_colour(colour_to_replace, new_colour):
             image.set_at((x, y), current_pixel_new)
             '''
 
+
+def replace_colour(colour_to_replace, new_colour, image):
+    for x in range(0, image.get_width()):
+        for y in range(0, image.get_height()):
+            if image.get_at((x, y)) == colour_to_replace:
+                image.set_at((x, y), new_colour)
+    return image
+
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
             running = False
 
+    replace_colour(colourToReplace, pygame.Color(0, 0, 0), image)
     screen.blit(image, (0, 0))
-
-    replace_colour(colourToReplace, pygame.Color(0, 0, 0))
 
     pygame.display.update()
     clock.tick(60)
