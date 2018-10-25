78d476d10350f6d0ef5dba7c8188ee05dfeaee6f
Cyrus Cleary <zeusofthecrows+github@gmail.com>
2018-10-25 23:57:29 +0100

    Refactored further, now has separate function rather than all in the main loop. Added a few comments (could use more?) Lines 61-64 are ugly, but I don't know how to fix that whilst adhering to pep8. Line 66 could be more efficient, but errored for some reason.

--- Contract1.py
+++ Contract1.py
@@ -11,14 +11,15 @@ for i in range(0, 15):
 
 roomImage = pygame.image.load(image_path[0])  # The first room image will
 # always be the basic layout, after that it will be random
-numberOfRoomImages = 5
+numberOfRoomImages = 5  # number of room images in the "Images" folder
 overlays = [pygame.image.load('Images/WaterOverlay.png'),
             pygame.image.load('Images/AirOverlay.png'),
             pygame.image.load('Images/EarthOverlay.png'),
             pygame.image.load('Images/FireOverlay.png')
             ]
 screen = pygame.display.set_mode((roomImage.get_width(),
-                                  roomImage.get_height()))
+                                  roomImage.get_height()
+                                  ))
 screen.fill((33, 33, 39))
 pygame.display.set_caption('Contract #1 Demonstration')
 
@@ -26,46 +27,46 @@ pygame.display.set_caption('Contract #1 Demonstration')
 screen.blit(roomImage, (0, 0))
 
 
-def main_game_loop():
+def apply_overlay(key_pressed):
     """
 
-          Main game loop
+          Applies an elemental overlay to the floor image
 
           Press W to display the water overlay,
           A for air, E for earth, and F for fire.
-          To remove the overlay , press N. This also displays
-          a new floor tile. Press Esc to exit.
+          To display a new floor tile, press N.
+          This also removed the overlay. Press Esc to exit.
 
               """
 
-    running = True
-    while running:
-
-        for event in pygame.event.get():
-            if \
-                    event.type == QUIT \
-                    or \
-                    (event.type == KEYUP and event.key == K_ESCAPE):
-                        running = False
-            elif event.type == KEYUP and event.key == K_w:
-                screen.blit(overlays[0], (0, 0))
-            elif event.type == KEYUP and event.key == K_a:
-                screen.blit(overlays[1], (0, 0))
-            elif event.type == KEYUP and event.key == K_e:
-                screen.blit(overlays[2], (0, 0))
-            elif event.type == KEYUP and event.key == K_f:
-                screen.blit(overlays[3], (0, 0))
-            elif event.type == KEYUP and event.key == K_n:
-                global roomImage
-                roomImage = pygame.image.load(image_path[random.randint
-                                            (0, numberOfRoomImages - 1)]
-                                            )
-                screen.blit(roomImage, (0, 0))
-
-        pygame.display.update()
-        clock.tick(60)
+    if key_pressed == K_w:
+        screen.blit(overlays[0], (0, 0))
+    elif key_pressed == K_a:
+        screen.blit(overlays[1], (0, 0))
+    elif key_pressed == K_e:
+        screen.blit(overlays[2], (0, 0))
+    elif key_pressed == K_f:
+        screen.blit(overlays[3], (0, 0))
+    elif key_pressed == K_n:
+        global roomImage  # This may not need to be global - please advise
+        roomImage = pygame.image.load(image_path[random.randint
+                                      (0, numberOfRoomImages - 1)]
+                                      )
+        screen.blit(roomImage, (0, 0))
 
 
-main_game_loop()
+running = True
+while running:
+    for event in pygame.event.get():
+        if \
+            event.type == QUIT \
+            or \
+                (event.type == KEYUP and event.key == K_ESCAPE):
+            running = False
+        elif event.type == KEYUP:  # if any key is pressed, launches function
+            apply_overlay(event.key)                        # (inefficient)
+
+    pygame.display.update()
+    clock.tick(60)
 
 pygame.quit()
