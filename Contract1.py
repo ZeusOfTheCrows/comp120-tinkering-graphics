8307e3c919e3e30c3f070c5cd6a3e6d7c18663e7
Cyrus Cleary <zeusofthecrows+github@gmail.com>
2018-10-25 15:27:30 +0100

    Edited layout to conform with pep8's 79 character line limit. Also changed some images to "final" versions. Contract1.py is now the main file once again.

--- Contract1.py
+++ Contract1.py
@@ -1,5 +1,4 @@
 import random
-
 import pygame
 from pygame.locals import *
 pygame.init()
@@ -8,37 +7,55 @@ clock = pygame.time.Clock()
 
 image_path = []
 for i in range(0, 15):
-    image_path.append(str('Images/Tilemap' + str(i) + '.png'))
+    image_path.append(str('Images/Room' + str(i) + '.png'))
 
-imageToRecolour = pygame.image.load(image_path[0])
-colourToBeReplaced = pygame.Color(128, 0, 128)
-newColours = {'Blood': (102, 0, 0), 'Mud': (144, 108, 63)}
-screen = pygame.display.set_mode((imageToRecolour.get_width(), imageToRecolour.get_height()))
+floorImage = pygame.image.load(image_path[0])
+numberOfFloorImages = 5
+overlays = [pygame.image.load('Images/WaterOverlay.png'),
+            pygame.image.load('Images/AirOverlay.png'),
+            pygame.image.load('Images/EarthOverlay.png'),
+            pygame.image.load('Images/FireOverlay.png')
+            ]
+screen = pygame.display.set_mode((floorImage.get_width(),
+                                  floorImage.get_height()))
 screen.fill((33, 33, 39))
 pygame.display.set_caption('Contract #1 Demonstration')
 
 
-def replace_colour(colour_to_be_replaced, replacement_colour, image):
-    for x in range(0, image.get_width()):
-        for y in range(0, image.get_height()):
-            if image.get_at((x, y)) == colour_to_be_replaced:
-                image.set_at((x, y), replacement_colour)
-    return image
-
+screen.blit(floorImage, (0, 0))
 
 running = True
 while running:  # Main game loop
-    for event in pygame.event.get():
-        if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
-            running = False
-        elif event.type == KEYUP and event.key == K_b:  # Press b for blood coloured splash
-            replace_colour(colourToBeReplaced, newColours['Blood'], imageToRecolour)
-        elif event.type == KEYUP and event.key == K_m:  # Press m for mud coloured splash
-            replace_colour(colourToBeReplaced, newColours['Mud'], imageToRecolour)
-        elif event.type == KEYUP and event.key == K_n:  # Press n for new tile
-            imageToRecolour = pygame.image.load(image_path[random.randint(0, 14)])
+    '''
+    
+    Main game loop
+    
+    Press W to display the water overlay,
+    A for air, E for earth, and F for fire.
+    To remove the overlay , press N. This also displays
+    a new floor tile. Press Esc to exit.
+    
+    '''
 
-    screen.blit(imageToRecolour, (0, 0))
+    for event in pygame.event.get():
+        if \
+                event.type == QUIT \
+                or \
+                (event.type == KEYUP and event.key == K_ESCAPE):
+                    running = False
+        elif event.type == KEYUP and event.key == K_w:
+            screen.blit(overlays[0], (0, 0))
+        elif event.type == KEYUP and event.key == K_a:
+            screen.blit(overlays[1], (0, 0))
+        elif event.type == KEYUP and event.key == K_e:
+            screen.blit(overlays[2], (0, 0))
+        elif event.type == KEYUP and event.key == K_f:
+            screen.blit(overlays[3], (0, 0))
+        elif event.type == KEYUP and event.key == K_n:
+            floorImage = pygame.image.load(image_path[random.randint
+                                           (0, numberOfFloorImages - 1)]
+                                           )
+            screen.blit(floorImage, (0, 0))
 
     pygame.display.update()
     clock.tick(60)
